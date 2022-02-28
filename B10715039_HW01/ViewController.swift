b35202fea84e0fff09bda8a9874bd323c4396afb
Roy <b10715039@ntust.edu.tw>
2022-02-28 21:16:45 +0800

    Initial Commit

---
+++ B10715039_HW01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  B10715039_HW01
+//
+//  Created by Jia-Ru Song on 2022/2/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
