b855d4f85aa7f86ec3f9a5f30412a9bbf8d23312
B10715039 <b10715039@ntust.edu.tw>
2022-03-02 11:06:54 +0800

    most of function complete

--- B10715039_HW01/ViewController.swift
+++ B10715039_HW01/ViewController.swift
@@ -7,21 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource ,UIPickerViewDelegate {
     
-    let symbol = ["🍏", "🍊", "🍌", "🍉", "🍇", "🍅", "🥝"]
+    let symbol = ["🍏", "🍊", "🍌",
+                  "🍉", "🍇", "🍅", "🥝"
+    ]
     var score = 0
     var col1 = [String]()
     var col2 = [String]()
     var col3 = [String]()
     @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var bingoLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBAction func spinButtonClick(_ sender: Any) {
+        pickerView.selectRow(Int(arc4random() % 100), inComponent: 0, animated: true)
+        pickerView.selectRow(Int(arc4random() % 100), inComponent: 1, animated: true)
+        pickerView.selectRow(Int(arc4random() % 100), inComponent: 2, animated: true)
+        if (col1[pickerView.selectedRow(inComponent: 0)] == col2[pickerView.selectedRow(inComponent: 1)] && col2[pickerView.selectedRow(inComponent: 1)] == col3[pickerView.selectedRow(inComponent: 2)]) {
+            bingoLabel.text = "Bingo!"
+        } else {
+            bingoLabel.text = "沒中獎!"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         for _ in 1...100 {
-            col1.append(symbol[(Int)(arc4random() % 7)])
-            col2.append(symbol[(Int)(arc4random() % 7)])
-            col3.append(symbol[(Int)(arc4random() % 7)])
+            col1.append(symbol[(Int)(Int(arc4random()) % symbol.count)])
+            col2.append(symbol[(Int)(Int(arc4random()) % symbol.count)])
+            col3.append(symbol[(Int)(Int(arc4random()) % symbol.count)])
         }
     }
 
@@ -46,6 +60,10 @@ class ViewController: UIViewController {
                 let acc = (alert.textFields?.first)! as UITextField
                 let pwd = (alert.textFields?.last)! as UITextField
                 if (acc.text!) == "" || (pwd.text!) == ""{
+                    let alert2 = UIAlertController(title: "", message: "請輸入帳號及密碼！", preferredStyle: .alert)
+                    alert2.addAction(UIAlertAction(title: "確定", style: .cancel))
+                    
+                    self.present(alert2, animated: true)
                     return
                 }
                 self.helloLabel.text = "您好，\(acc.text!)"
@@ -57,6 +75,35 @@ class ViewController: UIViewController {
         
         login()
     }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+                return col1.count
+        }
+        if component == 1 {
+            return col2.count
+        }
+        return col3.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return col1[row]
+        }
+        if component == 1 {
+            return col2[row]
+        }
+        return col3[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
 }
 
